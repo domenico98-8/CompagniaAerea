f4acc74a366f477df7c15999a763b3add9cb4690
domenico98-8 <cascelladomenico@gmail.com>
2024-12-30 15:58:27 +0100

    Abilitato il CORS, modificato il metodo di generazione dell'hash

--- src/main/java/com/cybersecurity/progetto_cybersecurity/controller/UtenteController.java
+++ src/main/java/com/cybersecurity/progetto_cybersecurity/controller/UtenteController.java
@@ -2,7 +2,6 @@ package com.cybersecurity.progetto_cybersecurity.controller;
 
 
 import com.cybersecurity.progetto_cybersecurity.controller.dto.UtenteDTO;
-import com.cybersecurity.progetto_cybersecurity.entity.Utente;
 import com.cybersecurity.progetto_cybersecurity.jwt.JwtUtil;
 import com.cybersecurity.progetto_cybersecurity.services.UtenteService;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -10,9 +9,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -34,12 +34,8 @@ public class UtenteController {
         if(utenteService.existUtente(utente.getEmail())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Utente già registrato con questa email.");
         }
-        // Hash della password usando BCrypt
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String hashedPassword = passwordEncoder.encode(utente.getPassword());
-
         // Imposta la password hashata nell'oggetto utente
-        utente.setPassword(hashedPassword);
+        utente.setPassword(generateSHA256Hash(utente.getPassword()));
         utenteService.saveUtente(utente);
         return ResponseEntity.status(HttpStatus.CREATED).body("Utente Creato");
     }
@@ -73,6 +69,28 @@ public class UtenteController {
             }
         }else{
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Utente non trovato!");
+        }
+    }
+
+    private static String generateSHA256Hash(String input) {
+        try {
+            // Ottieni l'istanza del digest SHA-256
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            // Calcola l'hash della stringa input
+            byte[] hash = digest.digest(input.getBytes());
+
+            // Converte l'array di byte in una stringa esadecimale
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                // Converti ogni byte in un valore esadecimale e aggiungilo alla stringa
+                hexString.append(String.format("%02x", b));
+            }
+
+            // Restituisce l'hash in formato esadecimale
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 algorithm not found.", e);
         }
     }
 
--- src/main/java/com/cybersecurity/progetto_cybersecurity/security/SecurityConfig.java
+++ src/main/java/com/cybersecurity/progetto_cybersecurity/security/SecurityConfig.java
@@ -4,15 +4,18 @@ import com.cybersecurity.progetto_cybersecurity.jwt.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
@@ -22,13 +25,15 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(new AntPathRequestMatcher("/api/utenti/login")).permitAll() // Consenti il login senza autenticazione
-                        .anyRequest().authenticated() // Tutte le altre richieste devono essere autenticati
-                )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .cors() // Abilita la configurazione CORS
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/utenti/login").permitAll()  // Consenti l'accesso senza autenticazione al login
+                .anyRequest().authenticated()  // Richiede autenticazione per tutte le altre richieste
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // Aggiungi il filtro JWT
 
         return http.build();
     }
